f74e73ea4988b660d62c37821d756d8ff6a05ca1
LuxianAiolos <luxianaiolos@gmail.com>
2023-02-21 12:04:17 -0800

    Created user Schema

---
+++ server/models/userModel.js
@@ -0,0 +1,23 @@
+const Schema = require('mongoose')
+
+const userSchema = mongoose.Schema(
+  {
+    user_name: {
+      type: String,
+      required: [ true, 'Please add name.'],
+    },
+    email: {
+      type: String,
+      required: [ true, 'Please add Email.'],
+    },
+    password: {
+      type: String,
+      required: [ true, 'Please add password.'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+module.exports = mongoose.model("User", userSchema)
