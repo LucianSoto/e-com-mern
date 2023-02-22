484870ac59559af07fcc852c16a19458d690e5f7
Lucian Brooks <74628091+LucianAiolos@users.noreply.github.com>
2023-02-22 14:57:15 -0800

    Nav bar Icons
    Animated hamburger icons library,
    and basic layout of nav.

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,24 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { Cross as Hamburger } from 'hamburger-react'
+import { BiSearchAlt } from 'react-icons/bi'
 
 const Nav = () => {
+  const [isOpen, setOpen] = useState(false)
+
   return (
     <nav className='flex '>
-      <div className="logo">LOGO</div>
-      <div className="flex ">
-        <div className="">Men</div>
-        <div className="">Women</div>
-        <div className="">Kids</div>
-        <div className="">Sale</div>
-      </div>
-      {/*  use a search icon */}
-      <input 
-        className=''
-        type="text" 
-        placeholder='Search...'
+      <div className="text-3xl">LOGO</div>
+      <Hamburger 
+        toggled={isOpen} 
+        toggle={setOpen}
+        className="text-black"
+        color="#000"
       />
-      <Link to="/log_in">Log In</Link>
-      <Link to="/register" >Sign Up</Link>
+      <div id="dropdown">
+        <div className="flex ">
+          <div className="">Men</div>
+          <div className="">Women</div>
+          <div className="">Kids</div>
+          <div className="">Sale</div>
+        </div>
+        {/*  use a search icon */}
+        <div id="search" className='flex'>
+          <input 
+            className='border-2 border-indigo-600 rounded-full pl-[10px]'
+            type="text" 
+            placeholder='Search...'
+          />
+          <BiSearchAlt className='mt-1 -ml-7 text-3xl text-gray-500'/>
+        </div>
+        <Link to="/log_in">Log In</Link>
+        <Link to="/register" >Sign Up</Link>
+      </div>
     </nav>
   )
 }
