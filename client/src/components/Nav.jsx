07f7674cd60113c9bc0bf2b3ab8f6eb1e6b7d984
Lucian Brooks <74628091+LucianAiolos@users.noreply.github.com>
2023-02-23 01:01:36 -0800

    Re adjusted Nap components
    and redid CSS for "side bar"

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -5,11 +5,20 @@ import { BiSearchAlt } from 'react-icons/bi'
 
 const Nav = () => {
   const [isOpen, setOpen] = useState(false)
+  // const [dropDown, setDropDown] = useState(false)
 
   return (
-    <nav className='flex'>
-      <div id="logo-burder-cont" className='flex'>
+    <nav className='flex-col'>
+      <div id="logo-burder-cont" className='flex w-full'>
         <div className="text-3xl">LOGO</div>
+        <div id="search" className='flex'>
+          <input 
+            className='border-b pl-[6px] h-10'
+            type="text" 
+            placeholder='Search...'
+          />
+          <BiSearchAlt className='mt-1 -ml-7 text-3xl text-gray-500'/>
+        </div>
         <Hamburger 
           toggled={isOpen} 
           toggle={setOpen}
@@ -17,23 +26,18 @@ const Nav = () => {
           color="#000"
         />
       </div>
-      <div id="dropdown" className='flex-col'>
-        <div id="search" className='flex'>
-          <input 
-            className='border-b pl-[10px]'
-            type="text" 
-            placeholder='Search...'
-          />
-          <BiSearchAlt className='mt-1 -ml-7 text-3xl text-gray-500'/>
-        </div>
-        <Link to="/log_in">Log In</Link>
-        <br />
-        <Link to="/register" >Sign Up</Link>
-          <div className="">Men</div>
-          <div className="">Women</div>
-          <div className="">Kids</div>
-          <div className="">Sale</div>
-      </div>
+      {
+        isOpen && 
+          <div id="dropdown" className='z-10 absolute bg-white flex-col w-2/4 text-2xl'>
+            <Link to="/log_in">Log In</Link>
+            <br />
+            <Link to="/register" >Sign Up</Link>
+              <div className="">Men</div>
+              <div className="">Women</div>
+              <div className="">Kids</div>
+              <div className="">Sale</div>
+          </div>
+      }
     </nav>
   )
 }
