d740226056e599dfe8e1b664c592c7a50aec44cd
LuxianAiolos <luxianaiolos@gmail.com>
2023-04-09 13:49:37 -0700

    password reset component

--- client/src/pages/PasswordReset.jsx
+++ client/src/pages/PasswordReset.jsx
@@ -3,22 +3,27 @@ import React, {useEffect, useState} from 'react'
 
 function PasswordReset() {
   const updatePassword = () => {
-    
+
   }
 
   return (
-    <div>
-      <p className='text-3xl'>Password Reset</p>
-      <form action="submit" onSubmit={()=> updatePassword()}>
+    <div className='h-screen flex flex-col items-center w-full'>
+      <p className='text-3xl my-10'>Password Reset</p>
+      <form action="submit" onSubmit={()=> updatePassword()}
+        className="w-full h-full p-4 "
+      >
         <p className="text-xl">New Password</p>
         <input type="text" 
-          className="" 
+          className="text-xl rounded mb-4" 
         />
         <p className="text-xl">Confirm Password</p>
         <input type="text" 
-          className="" 
+          className="text-xl rounded mb-4" 
         />
-        <button>Submit</button>
+        <br />
+        <button
+          className='text-2xl px-4 py-2 bg-green-500 rounded-md mt-8'
+        >Submit</button>
       </form>
     </div>
   )
--- server/utils/passwordReset.js
+++ server/utils/passwordReset.js
@@ -1,8 +1,10 @@
 const nodemailer = require('nodemailer')
 
-const passwordReset = async ( email, subject, text ) => {
+const passwordReset = async ( email, subject, link ) => {
   try {
-    console.log(email, subject, text )
+    console.log(email, subject, link )
+    console.log('***')
+    console.log(process.env.USER, )
     const transporter = nodemailer.createTransport({
       host: process.env.HOST,
       service: process.env.SERVICE,
@@ -13,6 +15,14 @@ const passwordReset = async ( email, subject, text ) => {
                 pass: process.env.PASS,
             },
     })
+    transporter.verify().then(console.log).catch(console.error)
+    await transporter.sendMail({
+      from: process.env.USER,
+      to: email,
+      subject: subject,
+      text: link,
+    })
+    console.log('email sent successfully')
   } catch(err) {
     console.log(err, 'email not sent')
   }
